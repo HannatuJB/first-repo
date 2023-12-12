c930176794092d085da4d2780b694f095e2b888a
Hanne <hannatubwalajoshua@gmail.com>
2023-12-12 18:25:06 +0100

    modified the paths

--- file_cleaner.sh
+++ file_cleaner.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
 # Directory containing files to be archived
-SOURCE="/path/to/source/directory"
+SOURCE="path to sourcedirectory"
 
-# Directory to move old files
-ARCHIVE="/path/to/archive/directory"
+# Directory to archive old files
+ARCHIVE="path to archive directory"
 
-# Number of days to considered old
+# Number of days to consider old
 DAYS_THRESHOLD=30
 
 # Print header
 echo "File Archiving Script"
-echo "-------------------------"
+echo "---------------------"
 
 # Check if source directory exists
 if [ -d "$SOURCE" ]; then
+
     # Create archive directory if it doesn't exist
     mkdir -p "$ARCHIVE"
+
  # Find files older than the specified threshold
     find "$SOURCE" -type f -mtime +"$DAYS_THRESHOLD" -exec mv {} "$ARCHIVE" \;
-
+ #where -mtime is the modification time 
     echo "File archiving complete!"
 else
     echo "Error: Source directory does not exist."
